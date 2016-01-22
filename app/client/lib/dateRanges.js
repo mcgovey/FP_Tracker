e9cf2d289c691a14b27d787b1c1ffd3ff5995861
kmcg8 <kmcg82004@gmail.com>
2016-01-21 20:03:04 -0500

    Updated Export

--- app/client/lib/dateRanges.js
+++ app/client/lib/dateRanges.js
@@ -1,19 +1,22 @@
 Template.dateRangeCont.onRendered(function () {
-	
+	//check if session variable exists
 	if (Session.get('dates')) {
+		//get session variable
 		var sessionDate = Session.get('dates');
+		//convert to moment format
 		var dates = {
 			startDate: moment(sessionDate.startDate,'YYYY-MM-DD'),
 			endDate: moment(sessionDate.endDate,'YYYY-MM-DD')
 		};
-		// console.log('exists',sessionDate);
 	} else{
-		// console.log('does not exist');
+		// if session var doesn't exist set to standard dates
 		var dates = {
 			startDate: moment().startOf('month').subtract(1, 'years'),
 			endDate: moment()
 		};
 	};
+
+	// for the input date range, use the daterangepicker function from the so-named library
 	$('input[name="daterange"]').daterangepicker(
 		{
 		locale: {
@@ -23,21 +26,24 @@ Template.dateRangeCont.onRendered(function () {
 		endDate: dates.endDate
 		}
 	);
+	// reformat dates into format that will be readable by mongo
 	var formattedDates = {
 		startDate: dates.startDate.format('YYYY-MM-DD'),
 		endDate: dates.endDate.format('YYYY-MM-DD')
 	};
+	// set the variable
 	Session.set('dates',formattedDates);
 });
 
 Template.dateRangeCont.events({
+	// on any change in the input field run the function to update the session var
 	'change #daterange' : function (event) {
 		var daterange = $('input[name="daterange"]').val().split(" - ");
 		var dates = {
 			startDate: moment(daterange[0],'MMM D YYYY').format('YYYY-MM-DD'),
 			endDate: moment(daterange[1],'MMM D YYYY').format('YYYY-MM-DD')
 		};
-		// console.log('dates',dates);
+		// set the updated session vars to the dates in the inputbox
 		Session.set('dates',dates);
 	}
 });
--- app/client/lib/exportCSV.js
+++ app/client/lib/exportCSV.js
@@ -1,17 +1,30 @@
 
 Template.leads.events({
 	'click .btnRawCSV' : function() {
-	    Router.go('exportSelections');
-	}
-});
-
-
-Template.exportSelections.events({
-	'click .exportBtn' : function() {
-	    var rawData = Leads.find({},{fields: { _id: 0 }}).fetch();
+	    // Router.go('exportSelections');
+		var dateRange = Session.get('dates');
+	    var rawData = Leads.find({ inquiryDate: {
+				$gte: dateRange.startDate,
+				$lt: dateRange.endDate
+			} },{fields: { _id: 0 }}).fetch();
 	    csv = Papa.unparse(rawData);//json2csv( rawData, true, true );
 	    var blob = new Blob([csv], {type: "text/plain;charset=utf-8;",});
 
-	    saveAs(blob, "allfp.csv");    
+	    saveAs(blob, "fosterparents.csv"); 	    
 	}
 });
+
+
+// Template.exportSelections.events({
+// 	'click .exportBtn' : function() {
+// 		var dateRange = Session.get('dates');
+// 	    var rawData = Leads.find({ inquiryDate: {
+// 				$gte: dateRange.startDate,
+// 				$lt: dateRange.endDate
+// 			} },{fields: { _id: 0 }}).fetch();
+// 	    csv = Papa.unparse(rawData);//json2csv( rawData, true, true );
+// 	    var blob = new Blob([csv], {type: "text/plain;charset=utf-8;",});
+
+// 	    saveAs(blob, "allfp.csv");    
+// 	}
+// });
